be79fca642028774890174b0c9e9c14130e4d053
AntonioNtV <antoniontv18@gmail.com>
2020-08-24 11:48:07 -0300

    Adicioandno funcionalidade de adicionar novos projetos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native'
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native'
 import api from './services/api'
 
 /**
@@ -20,34 +20,65 @@ export default function App() {
         })
     }, [])
 
+    async function handleAddProject() {
+        const response = await api.post('/projects', {
+            title: 'Novo Projeto Adicionado',
+            owner: 'Natalia lindona'
+          })
+
+          setProjects([...projects, response.data])
+    }
+
     return (
         <>
-            <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
+            <StatusBar barStyle="light-content" backgroundColor="#c3c3c3c3"/>
             <SafeAreaView style={styles.container}>
+                <Text style={styles.title}>Projetos</Text>
                 <FlatList 
-                data={projects} 
+                data={projects}
+                showsHorizontalScrollIndicator={false}
+                showsVerticalScrollIndicator={false}
                 keyExtractor={project => project.id}
                 renderItem={({ item: project }) => (
                 <Text style={styles.project}>{project.title}</Text>
                 )}
                 />
+                <TouchableOpacity style={styles.button} onPress={handleAddProject}>
+                    <Text style={styles.buttonText}>Adicionar projeto</Text>
+                </TouchableOpacity>
             </SafeAreaView>
         </>
     )
 }
 
 const styles = StyleSheet.create({
-    status: {
-        backgroundColor: '#7159c1'
-    },
     container: {
         flex: 1,
-        backgroundColor: '#7159c1',
+        backgroundColor: '#c3c3c3c3',
         alignItems: "center",
-        justifyContent: "center"
+        justifyContent: "center",
     },
     project: {
+        color: '#7159c1',
+        fontSize: 20,
+    },
+    title: {
+        color: '#7159c1',
+        fontSize: 30,
+        fontWeight: 'bold'
+    },
+    buttonText: {
         color: '#FFF',
         fontSize: 20,
+        fontWeight: 'bold'
+    },
+    button: {
+        backgroundColor: '#7159c1',
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 20,
+        width: '50%',
+        height: '7%',
+        marginBottom: 20
     }
 })
