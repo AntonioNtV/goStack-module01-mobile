9dc79b9b738e852c91f9d611e85a89144a14ec9b
AntonioNtV <antoniontv18@gmail.com>
2020-08-24 10:09:33 -0300

    Teste de uso do react native

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text, StyleSheet, StatusBar } from 'react-native'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
+
+/**
+ *  Não possuem valor semâtico (não possui significado)
+ *  Não possui estilização própria
+ *  Todos componentes possuem por padrao display flex
+ * 
+ *  View: div, footer, header, main, aside, section... (container)
+ *  Text: p, span, strong, h1, h2, h3
+ */
 
 export default function App() {
-    return <View/>
+    return (
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
+            <View style={styles.container}>
+                <Text style={styles.title}> Hello GoStack </Text>
+            </View>
+        </>
+    )
 }
+
+const styles = StyleSheet.create({
+    status: {
+        backgroundColor: '#7159c1'
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#7159c1',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 30,
+        color: '#ffffff',
+        fontWeight: 'bold'
+    }
+})
