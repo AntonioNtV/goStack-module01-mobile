de3b6047427542be369ac59eb7a084f8804bc4ca
AntonioNtV <antoniontv18@gmail.com>
2020-08-24 10:24:00 -0300

    Adicioando service para se responsabilizar com o contato com a api

--- src/services/api.js
+++ src/services/api.js
@@ -3,3 +3,10 @@ import axios from 'axios'
 export default api = axios.create({
     baseURL: 'http://localhost:3333'
 })
+/**
+ * iOs com Emulador: localhost
+ * iOs com fisico: Ip da Maquina
+ * Android com Emulador: adb reverse
+ * Android com Emulador: Usar Ip especifico 
+ * Android com fisico: Ip da maquina
+ */
